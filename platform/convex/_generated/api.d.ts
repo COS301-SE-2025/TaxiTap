9d97517bb82e9a90b04e485a80ac3e89ab1a38de
habohamese <u21532941@tuks.co.za>
2025-07-29 11:30:57 +0200

    Updated schema and api functions

--- platform/convex/_generated/api.d.ts
+++ platform/convex/_generated/api.d.ts
@@ -47,8 +47,12 @@ import type * as functions_rides_declineRide from "../functions/rides/declineRid
 import type * as functions_rides_declineRideHandler from "../functions/rides/declineRideHandler.js";
 import type * as functions_rides_endRide from "../functions/rides/endRide.js";
 import type * as functions_rides_endRideHandler from "../functions/rides/endRideHandler.js";
+import type * as functions_rides_generatePin from "../functions/rides/generatePin.js";
 import type * as functions_rides_getRideById from "../functions/rides/getRideById.js";
+import type * as functions_rides_getRidePin from "../functions/rides/getRidePin.js";
 import type * as functions_rides_startRide from "../functions/rides/startRide.js";
+import type * as functions_rides_verifyPin from "../functions/rides/verifyPin.js";
+import type * as functions_rides_verifyPinSimple from "../functions/rides/verifyPinSimple.js";
 import type * as functions_routes_calculateRoute from "../functions/routes/calculateRoute.js";
 import type * as functions_routes_displayRoutes from "../functions/routes/displayRoutes.js";
 import type * as functions_routes_enhancedTaxiMatching from "../functions/routes/enhancedTaxiMatching.js";
@@ -81,7 +85,6 @@ import type * as functions_users_UserManagement_updateUserProfile from "../funct
 import type * as functions_users_UserManagement_updateWorkAddress from "../functions/users/UserManagement/updateWorkAddress.js";
 import type * as functions_work_sessions_endWorkSession from "../functions/work_sessions/endWorkSession.js";
 import type * as functions_work_sessions_startWorkSession from "../functions/work_sessions/startWorkSession.js";
-
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -126,8 +129,12 @@ declare const fullApi: ApiFromModules<{
   "functions/rides/declineRideHandler": typeof functions_rides_declineRideHandler;
   "functions/rides/endRide": typeof functions_rides_endRide;
   "functions/rides/endRideHandler": typeof functions_rides_endRideHandler;
+  "functions/rides/generatePin": typeof functions_rides_generatePin;
   "functions/rides/getRideById": typeof functions_rides_getRideById;
+  "functions/rides/getRidePin": typeof functions_rides_getRidePin;
   "functions/rides/startRide": typeof functions_rides_startRide;
+  "functions/rides/verifyPin": typeof functions_rides_verifyPin;
+  "functions/rides/verifyPinSimple": typeof functions_rides_verifyPinSimple;
   "functions/routes/calculateRoute": typeof functions_routes_calculateRoute;
   "functions/routes/displayRoutes": typeof functions_routes_displayRoutes;
   "functions/routes/enhancedTaxiMatching": typeof functions_routes_enhancedTaxiMatching;
