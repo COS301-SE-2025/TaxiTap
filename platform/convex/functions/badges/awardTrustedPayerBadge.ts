9b3c4745f6943487f7dfa8ef42ee06a1bef535a8
un4thi <u22553402@tuks.co.za>
2025-09-18 11:38:03 +0200

    badge functions for testung eligibility and awarding badge

---
+++ platform/convex/functions/badges/awardTrustedPayerBadge.ts
@@ -0,0 +1,13 @@
+import { mutation } from "../../_generated/server";
+import { v } from "convex/values";
+import { checkAndAwardTrustedPayerBadge } from "./badgeService";
+
+export const awardTrustedPayerBadge = mutation({
+  args: {
+    userId: v.id("taxiTap_users"),
+  },
+  handler: async (ctx, args) => {
+    return await checkAndAwardTrustedPayerBadge(ctx, args.userId);
+  },
+});
+
---
+++ platform/convex/functions/badges/checkTrustedPayerEligibility.ts
@@ -0,0 +1,13 @@
+import { query } from "../../_generated/server";
+import { v } from "convex/values";
+import { checkTrustedPayerEligibility } from "./badgeService";
+
+export const checkTrustedPayerEligibilityQuery = query({
+  args: {
+    userId: v.id("taxiTap_users"),
+  },
+  handler: async (ctx, args) => {
+    return await checkTrustedPayerEligibility(ctx, args.userId);
+  },
+});
+
